b2727cfa0c194ca011f1cfee05f600aacc4bd74c
Sagar k.c <sagarkc45172@gmail.com>
2022-10-15 21:01:29 +0545

    Day 2

--- list.py
+++ list.py
@@ -1,6 +1,25 @@
-c=[12,"hello",34,12.32]
+c=[12,46,34,12.32,82]
 print(c)
 
 print(c[0])
 
 print(c[0:3])
+
+
+c.sort()
+print(c)
+
+c.reverse()
+print(c)
+
+c.append(92)
+print(c)
+
+c.insert(3,64)
+print(c)
+
+c.pop(3)
+print(c)
+
+c.remove(12)
+print(c)
