2d25ceb1bb462e82f8277981e2e3483cc382bf84
Sagar k.c <sagarkc45172@gmail.com>
2022-10-15 20:15:09 +0545

    Day 2

---
+++ list.py
@@ -0,0 +1,6 @@
+c=[12,"hello",34,12.32]
+print(c)
+
+print(c[0])
+
+print(c[0:3])
---
+++ variables.py
@@ -0,0 +1,15 @@
+a=12
+b="Sagar"
+c=23.21
+
+print(a)
+print(b)
+print(c)
+
+print(a+13)
+
+print(type(a))
+
+print(type(b))
+
+print(type(c))
